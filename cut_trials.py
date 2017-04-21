33f7704ca612c8e702076bf84e82b2d8a5e09a41
cyxomo <cyxomo@gmail.com>
2017-04-21 11:14:07 +0800

    add cut_trials

---
+++ cut_trials.py
@@ -0,0 +1,22 @@
+import sys
+
+def fun(fin, trials, fout):
+	uttlist = []
+	with open(fin, 'r') as fi:
+		for line in fi:
+			part=line.split()
+			uttlist.append(part[0])
+	with open(fout, 'w') as fo:
+		with open(trials, 'r') as tr:
+			for line in tr:
+				part = line.split()
+				if part[1] in uttlist:
+					fo.write(line)
+
+if __name__ == '__main__':
+	num = 1
+	fin = '/work1/lilt/kaldi-161111/egs/sre08/v5/exp_cnn/fisher_2000/nnet3/cnn_4_8_pi2000_po400_l6_splice10/eva_female/dvector.'+ str(num)+'.scp'
+	trials = '/work1/lilt/kaldi-161111/egs/sre08/v5/data_fbank/fisher_test/female/eva_female/trials-1.trl'
+	fout = '/work7/lilt/170421-dvector/ver/frame2utt/trials/female/trials_1.trl'
+
+	fun(fin, trials, fout)
