aeee1c9b7efb8e7858d64ba96467bc1c16c81012
cyxomo <cyxomo@gmail.com>
2017-04-21 12:50:16 +0800

    wu

--- cut_trials.py
+++ cut_trials.py
@@ -1,22 +1,36 @@
 import sys
 
-def fun(fin, trials, fout):
+def fun(finpp, trials, foutpp):
+
 	uttlist = []
-	with open(fin, 'r') as fi:
-		for line in fi:
-			part=line.split()
-			uttlist.append(part[0])
-	with open(fout, 'w') as fo:
-		with open(trials, 'r') as tr:
-			for line in tr:
-				part = line.split()
-				if part[1] in uttlist:
-					fo.write(line)
+	foutlist = []
+	for i in range(20):
+		ii = i+1
+		uttlist.append([])
+
+	for i in range(20):
+		ii = i+1
+		fin = finpp + str(ii) +'.scp'
+		with open(fin, 'r') as fi:
+			for line in fi:
+				part=line.split()
+				uttlist[i].append(part[0])
+		fout = foutpp + str(ii) + '.trl'
+		foo = open(fout, 'w')
+		foutlist.append(foo)
+
+	with open(trials, 'r') as tr:
+		for line in tr:
+			part = line.split()
+			for i in range(20):
+				if part[1] in uttlist[i]:
+					foutlist[i].write(line)
+					continue
 
 if __name__ == '__main__':
-	num = 1
-	fin = '/work1/lilt/kaldi-161111/egs/sre08/v5/exp_cnn/fisher_2000/nnet3/cnn_4_8_pi2000_po400_l6_splice10/eva_female/dvector.'+ str(num)+'.scp'
+	num = nn+1
+	fin = '/work1/lilt/kaldi-161111/egs/sre08/v5/exp_cnn/fisher_2000/nnet3/cnn_4_8_pi2000_po400_l6_splice10/eva_female/dvector.'
 	trials = '/work1/lilt/kaldi-161111/egs/sre08/v5/data_fbank/fisher_test/female/eva_female/trials-1.trl'
-	fout = '/work7/lilt/170421-dvector/ver/frame2utt/trials/female/trials_1.trl'
+	fout = '/work7/lilt/170421-dvector/ver/frame2utt/trials/shishi/trials_'
 
 	fun(fin, trials, fout)
